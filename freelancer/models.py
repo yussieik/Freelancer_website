223ed02b7515150f827cd7852f2c99fa631c976d
DmitryArtamonov <artamonov2009@gmail.com>
2023-07-09 17:57:04 +0300

    models created

---
+++ freelancer/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+SCORES_CHOICES = (
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5)
+)
+
+class User(AbstractUser):
+    pass
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Freelancer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="freelancer_profile")
+    details = models.TextField(null=True, blank=True)
+    regions = models.ManyToManyField(Region)
+    services = models.ManyToManyField(Service)
+
+
+class Review(models.Model):
+    score = models.IntegerField(choices=SCORES_CHOICES)
+    text = models.TextField(null=True, blank=True)
+    freelancer = models.ForeignKey(Freelancer, on_delete=models.CASCADE, related_name="reviews")
+
